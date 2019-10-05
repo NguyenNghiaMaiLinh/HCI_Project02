9faf471ff1663605efe9f5b28b0a89a8d4c3e6b3
Megalodon <nguyennghiamailinhlmht@gmail.com>
2019-10-05 20:52:07 +0700

    first commit

---
+++ app/src/main/java/com/example/appstore/FormRegister.java
@@ -0,0 +1,67 @@
+package com.example.appstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class FormRegister extends AppCompatActivity {
+    EditText ten;
+    EditText ten1;
+    EditText dienthoai;
+    EditText email1;
+    Button button;
+    Spinner spinner1;
+    Spinner spinner2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_register);
+         spinner1 = (Spinner) findViewById(R.id.spinner1);
+         button = findViewById(R.id.btnSubmit);
+         ten = (EditText) findViewById(R.id.ten);
+         ten1 = (EditText) findViewById(R.id.ten1);
+         dienthoai = (EditText) findViewById(R.id.dienthoai);
+         email1 = (EditText) findViewById(R.id.email);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(FormRegister.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+         spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(FormRegister.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.danhnghieps));
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+    }
+
+    public void onSubmit1(View view) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (ten.getText().toString().isEmpty() ) {
+           ten.setError("Tên doanh nghiêp đang rỗng");
+
+        } else if (ten1.getText().toString().isEmpty()) {
+            ten1.setError("Người đại diện đang rỗng");
+        } else if (dienthoai.getText().toString().isEmpty()) {
+            dienthoai.setError("Số điện thoại đang rỗng");
+        } else if (email1.getText().toString().isEmpty()) {
+            email1.setError("Email không đang rỗng");
+        } else if (!email1.getText().toString().matches(emailPattern)) {
+            Toast.makeText(this, "Email không hợp lệ", Toast.LENGTH_LONG).show();
+        } else if (!spinner1.callOnClick()) {
+            Toast.makeText(this, "Thành phố chưa được chọn", Toast.LENGTH_LONG).show();
+        } else if (!spinner2.callOnClick()) {
+            Toast.makeText(this, "Loại hình doanh nghiệp chưa được chọn", Toast.LENGTH_LONG).show();
+        } else {
+             Toast.makeText(FormRegister.this, "Thành công", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appstore/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.appstore;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kaopiz.kprogresshud.KProgressHUD;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userEmail;
+    private EditText userPass;
+    private ProgressDialog progress;
+    private static int REGISTER_ACTIVITY = 1;
+    private static String ADMIN = "admin";
+    private static String PASS = "admin123";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userEmail = findViewById(R.id.userName1);
+        userPass = findViewById(R.id.userPassword1);
+    }
+    public void onClickHomePage(View view) {
+        String email = userEmail.getText().toString().trim();
+        String pass = userPass.getText().toString().trim();
+        if (TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(pass).matches()) {
+            Toast.makeText(LoginActivity.this, "Tên đăng nhập không được trống", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(LoginActivity.this, "Mật khẩu không được trống", Toast.LENGTH_LONG).show();
+        } else if (pass.length() < 6) {
+            Toast.makeText(LoginActivity.this, "Mật khẩu phải lớn hơn 6 ký tự", Toast.LENGTH_LONG).show();
+        } else if (email.equalsIgnoreCase(ADMIN) && pass.equalsIgnoreCase(PASS)) {
+            final KProgressHUD kProgressHUD = KProgressHUDManager.showProgessBar(LoginActivity.this, "Thành công");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    onCreateDialog();
+                    kProgressHUD.dismiss();
+
+                    // Toast.makeText(Login_Form.this, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                }
+            }, 1000);// = 1 seconds
+
+        } else {
+            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected Dialog onCreateDialog() {
+        return new AlertDialog.Builder(this)
+                .setView(R.layout.activity_dialog)
+                .show();
+    }
+    public  void onRegister(View view ){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivityForResult(intent, REGISTER_ACTIVITY);
+    }
+
+    public  void onGoogle(View view ){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivityForResult(intent, REGISTER_ACTIVITY);
+    }
+
+    public void onLienHe(View view){
+        Uri uri = Uri.parse("tel:1900 5656");
+        Intent intent1 = new Intent(Intent.ACTION_VIEW, uri);
+        //Intent intent1 = new Intent(Intent.ACTION_DIAL, uri);
+        // Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent1);
+    }
+}
