fa8c300d7abab1dc34abb17bbaca1bb083a47e34
phuocchau2210 <vodiepphuocchau@gmail.com>
2019-10-10 15:23:31 +0700

    ref booking page

--- app/src/main/java/com/example/appstore/RejectBookingReasonDialog.java
+++ app/src/main/java/com/example/appstore/RejectBookingReasonDialog.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.View;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class RejectBookingReasonDialog extends Dialog implements
         android.view.View.OnClickListener {
